79ff674edfb48169de2527550a739690ca6217d1
Diego Garcia <dili02@gmail.com>
2024-02-24 16:50:17 -0300

    fix error categories

--- src/app/dashboard/categorias/category-list.tsx
+++ src/app/dashboard/categorias/category-list.tsx
@@ -2,6 +2,7 @@ import { deleteCategoryById } from "@/actions/category.action";
 import ButtonDeleteIcon from "@/components/button-delete-icon";
 import { Button } from "@/components/ui/button";
 import { getCategories } from "@/fetch/category.fetch";
+import { Category } from "@prisma/client";
 import { Edit, FileWarning } from "lucide-react";
 import Link from "next/link";
 import React from "react";
@@ -29,7 +30,7 @@ export default async function CategoryList({ query, userId }: Props) {
       )}
 
       <div className="grid grid-cols-1 gap-5 md:grid-cols-2 transition-all xl:grid-cols-3">
-        {categories.map((category) => (
+        {categories.map((category: Category) => (
           <article
             key={category.id}
             className="flex justify-between flex-grow gap-3 border rounded-xl p-5 bg-muted overflow-hidden md:flex-col"
